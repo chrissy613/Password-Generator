9672f39c791e8da4ef4287c9bebacf55a53f5649
Chris <cec213@scarletmail.rutgers.edu>
2020-07-14 13:57:32 -0400

    added variables and arrays

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var letters;
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers;
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChars;
+specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ".", ",", "'", "<", ">", "/", "?"];
 
 
 // Write password to the #password input
