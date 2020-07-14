b26d3bb9149291e76c1f6e4b5fd7a91eb26e0b36
Chris <cec213@scarletmail.rutgers.edu>
2020-07-14 16:12:36 -0400

    readme update and new statements

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password-Generator
+I was very overwhelmed with this assignment but I tried my best. 
+I started by adding the variables for the password itself, the letters, numbers, and special characters. 
+Then I added the prompt for integer input and the confirms for the rest of the variables. 
+It's at this point that I got stumped for hours trying different if, else if, and else statements because sometimes some of them caused the button to do nothing or not what I wanted. 
+
+
--- script.js
+++ script.js
@@ -7,18 +7,24 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars;
 specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ".", ",", "'", "<", ">", "/", "?"];
 
-
+var options
 // Write password to the #password input
 function writePassword() {
-  prompt("Your password can be 8 to 128 characters long. How many characters would you like?")
-  confirm("Would you like your password to contain uppercase letters?")
-  confirm("Would you like your password to contain lowercase letters?")
-  confirm("Would you like your password to contain numbers?")
-  confirm("Would you like your password to contain special characters?")
-  var password = generatePassword();
+  numberImput = prompt("Your password can be 8 to 128 characters long. How many characters would you like?");
+  if (!numberImput) {
+    alert("Value necessary to proceed.")
+  } else if (numberImput < 8 || numberImput > 128) {
+    numberImput = prompt("An integer between 8 and 128 is required.")
+  } else {
+    confirmUpper = confirm("Would you like your password to contain uppercase letters?");
+    confirmLower = confirm("Would you like your password to contain lowercase letters?");
+    confirmNumbers = confirm("Would you like your password to contain numbers?");
+    confirmSpecials = confirm("Would you like your password to contain special characters?");
+  }
+  var password = generatePassword(letters, numbers, specialChars);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  password = letters + numbers + specialChars;
+  return passwordText;
 }
 
 // Add event listener to generate button
