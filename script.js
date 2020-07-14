b8fe98f9b1092534df0860f568f4bcd2de89033d
Chris <cec213@scarletmail.rutgers.edu>
2020-07-14 17:04:26 -0400

    more if else statements

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ var numbers;
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars;
 specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ".", ",", "'", "<", ">", "/", "?"];
-
 var options
+
 // Write password to the #password input
 function writePassword() {
+  //PROMPT AND CONFIRMS
   numberImput = prompt("Your password can be 8 to 128 characters long. How many characters would you like?");
   if (!numberImput) {
     alert("Value necessary to proceed.")
@@ -18,13 +19,64 @@ function writePassword() {
   } else {
     confirmUpper = confirm("Would you like your password to contain uppercase letters?");
     confirmLower = confirm("Would you like your password to contain lowercase letters?");
-    confirmNumbers = confirm("Would you like your password to contain numbers?");
+    confirmNumber = confirm("Would you like your password to contain numbers?");
     confirmSpecials = confirm("Would you like your password to contain special characters?");
   }
-  var password = generatePassword(letters, numbers, specialChars);
+  //ELSE IF WITH ONE OPTION SELECTED
+  if (!confirmUpper && !confirmLower && !confirmNumber && !confirmSpecials) {
+    alert("You cannot create a password without choosing options.");
+  }
+  else if (confirmUpper && !confirmLower && !confirmNumber && !confirmSpecials) {
+    alert("Password created with uppercase letters.");
+  }
+  else if (!confirmUpper && confirmLower && !confirmNumber && !confirmSpecials) {
+    alert("Password created with lowercase letters.");
+  }
+  else if (!confirmUpper && !confirmLower && confirmNumber && !confirmSpecials) {
+    alert("Password created with numbers.");
+  }
+  else if (!confirmUpper && !confirmLower && !confirmNumber && confirmSpecials) {
+    alert("Password created with special characters.");
+  }
+  //ELSE IF WITH TWO OPTIONS SELECTED
+  else if (confirmUpper && confirmLower && !confirmNumber && !confirmSpecials) {
+    alert("Password created with uppercase and lowercase letters");
+  }
+  else if (confirmUpper && !confirmLower && confirmNumber && !confirmSpecials) {
+    alert("Password created with uppercase letters and numbers.");
+  }
+  else if (confirmUpper && !confirmLower && !confirmNumber && confirmSpecials) {
+    alert("Password created with uppercase letters and special characters.");
+  }
+  else if (!confirmUpper && confirmLower && confirmNumber && !confirmSpecials) {
+    alert("Password created with lowercase letters and numbers.");
+  }
+  else if (!confirmUpper && confirmLower && !confirmNumber && confirmSpecials) {
+    alert("Password created with lowercase letters and special characters.");
+  }
+  else if (!confirmUpper && !confirmLower && confirmNumber && confirmSpecials) {
+    alert("Password created with numbers and special characters.");
+  }
+  //ELSE IF WITH THREE OPTIONS SELECTED
+  else if (confirmUpper && confirmLower && confirmNumber && !confirmSpecials) {
+    alert("Password created with uppercase letters, lowercase letters, and numbers.");
+  }
+  else if (confirmUpper && confirmLower && !confirmNumber && confirmSpecials) {
+    alert("Password created with uppercase letters, lowercase letters, and special characters.");
+  }
+  else if (confirmUpper && !confirmLower && confirmNumber && confirmSpecials) {
+    alert("Password created with uppercase letters, numbers and special characters.");
+  }
+  else if (!confirmUpper && confirmLower && confirmNumber && confirmSpecials) {
+    alert("Password created with lowercase letters, numbers, and special characters.");
+  }
+  //ALL OPTIONS SELECTED
+  else if (confirmUpper && confirmLower && confirmNumber && confirmSpecials) {
+    alert("Password created with all options.");
+  }
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  return passwordText;
 }
 
 // Add event listener to generate button
